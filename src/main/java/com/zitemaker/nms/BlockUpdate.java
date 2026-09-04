60be384129fa7d1d94c10cd5d5bfee7cbc9b9f9b
lunarenzo <lunarenzo@users.noreply.github.com>
2026-09-05 05:07:38 +0800

    refactor: convert BlockUpdate to Java 21 record and remove FQCN

--- src/main/java/com/zitemaker/nms/BlockUpdate.java
+++ src/main/java/com/zitemaker/nms/BlockUpdate.java
@@ -1,18 +1,13 @@
 package com.zitemaker.nms;
 
-public class BlockUpdate {
-    private final int x, y, z;
-    private final org.bukkit.block.data.BlockData blockData;
+import org.bukkit.block.data.BlockData;
 
-    public BlockUpdate(int x, int y, int z, org.bukkit.block.data.BlockData blockData) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.blockData = blockData;
-    }
-
+/**
+ * Immutable data carrier for high-throughput NMS block updates.
+ */
+public record BlockUpdate(int x, int y, int z, BlockData blockData) {
     public int getX() { return x; }
     public int getY() { return y; }
     public int getZ() { return z; }
-    public org.bukkit.block.data.BlockData getBlockData() { return blockData; }
+    public BlockData getBlockData() { return blockData; }
 }
